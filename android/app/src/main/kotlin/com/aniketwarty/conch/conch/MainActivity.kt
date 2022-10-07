49e774fca4a7dad2988983020dd5ebe5135a32d5
Warty <aniket.warty25@bcp.org>
2022-10-06 19:49:44 -0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/aniketwarty/conch/conch/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.aniketwarty.conch.conch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Conch());
+}
+
+class Conch extends StatelessWidget {
+  const Conch({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
